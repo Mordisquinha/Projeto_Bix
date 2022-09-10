8a1a1d00202d5960cd0cb853e1254526ab59fa62
gsantiago <gabrielsantiagoinv@gmail.com>
2022-09-10 20:43:53 -0300

    var

--- ETAPA_STG/database.py
+++ ETAPA_STG/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(os.environ['ENGINE_SQLALCHEMY_PESSOAL'],
+engine = create_engine(os.environ['ENGINE_SQLALCHEMY_PESSOAL_STG'],
     echo=True)
 
 Base = declarative_base()
