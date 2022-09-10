890c4d298b27f67e7245a1e2433683439e1edefb
gsantiago <gabrielsantiagoinv@gmail.com>
2022-09-10 12:52:02 -0300

    DAG FEITA

--- dag.py
+++ dag.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.operators.bash import BashOperator
 from airflow.operators.subdag_operator import SubDagOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 
@@ -25,7 +25,7 @@ def load_subdag_stg(parent_dag_name, child_dag_name):
     with subdag_stg:
         for etapa in ['api.py', 'parquet.py', 'postgress.py']:
             command = f"""
-            cd ~/PROJETO_BIX/ETAPA_STG/; sudo python3 {etapa}"""
+            cd /home/ubuntu/Projeto_Bix/ETAPA_STG/; sudo python3 {etapa}"""
 
             if etapa == 'api.py':
                 stage1 = SSHOperator(
@@ -41,9 +41,9 @@ def load_subdag_stg(parent_dag_name, child_dag_name):
                     command = command,
                     dag = subdag_stg
                 )
-            if etapa == 'postgress.py'.replace('.py', ''):
+            if etapa == 'postgress.py':
                 stage3 = SSHOperator(
-                    task_id = f'{etapa}',
+                    task_id = f'{etapa}'.replace('.py', ''),
                     ssh_conn_id = 'ssh_servidor_bix',
                     command = command,
                     dag = subdag_stg
@@ -66,7 +66,7 @@ def load_subdag_rel(parent_dag_name, child_dag_name):
 
     with subdag_rel:
         command = """
-            cd ~/PROJETO_BIX/ETAPA_STG/; sudo python3 insert_rel.py"""
+            cd /home/ubuntu/Projeto_Bix/ETAPA_REL/; sudo python3 insert_rel.py"""
 
         rel = SSHOperator(
             task_id = 'insert_rel',
@@ -83,10 +83,10 @@ start = datetime.today() - timedelta(days=2)
 
 with DAG(dag_id="ETL_BIX", start_date=start,
         schedule_interval="0 7 * * *" , 
-        catchup=False):
+        catchup=False) as dag:
 
     stage = SubDagOperator(
-        task_id = 'STAGE',
+        task_id = 'STG',
         subdag = load_subdag_stg(
             parent_dag_name="ETL_BIX",
             child_dag_name="STG"
@@ -94,12 +94,15 @@ with DAG(dag_id="ETL_BIX", start_date=start,
     )
     
     rel = SubDagOperator(
-        task_id = 'RELACIONAL',
+        task_id = 'REL',
         subdag = load_subdag_rel(
             parent_dag_name="ETL_BIX",
             child_dag_name="REL"
         )
     )
+
+if __name__ == "__main__":
+    dag.cli()
 
 stage >> rel
 
